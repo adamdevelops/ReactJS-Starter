1e126d48843a28f6dc1fc5638f91fba16c1fba88
Adam <adamh93@verizon.net>
2017-08-01 14:03:52 -0400

    Resolved previous error by specifying where to render App instance on the page (in the div with class called container)

--- src/index.js
+++ src/index.js
@@ -10,5 +10,8 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
+
 //Can also be done as '<App></App>'
+// document.querySelector() used to find div with class 'container'
+// then render the App component within the div with class 'container'
